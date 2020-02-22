309190555a869b9fc9ec6de0193c7283ff7425f2
Don Mallory <dmm333@gmail.com>
2020-02-22 02:38:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   reversed_words = []
   words = sentence.split(" ")
-  words.each {|word| reversed_words << word.reverse}
+  words.collect {|word| reversed_words << word.reverse}
   return reversed_words.join(" ")
 end
